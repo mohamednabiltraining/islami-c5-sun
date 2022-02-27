62901b8f5be10deec4652295c14081dd83866d81
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2022-02-27 18:50:45 +0200

    created initial app added home screen

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:islami_c5_sun/home_screen.dart';
+
+void main() {
+  runApp(MyApplication());
+}
+
+class MyApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Islami',
+      routes: {HomeScreen.routeName: (buildContext) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
