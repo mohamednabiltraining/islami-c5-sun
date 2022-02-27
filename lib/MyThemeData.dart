4686fbc8dee0988ca930ce26880d815818c79ee5
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2022-02-27 20:37:36 +0200

    created home tabs added quran tab with sura names

---
+++ lib/MyThemeData.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyThemeData {
+  static final Color colorGold = Color(0xFFB7935F);
+  static final ThemeData theme = ThemeData(
+      primaryColor: colorGold,
+      scaffoldBackgroundColor: Colors.transparent,
+      appBarTheme: AppBarTheme(
+          color: Colors.transparent, elevation: 0, centerTitle: true),
+      textTheme: TextTheme(
+          headline1: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+          subtitle1: TextStyle(fontSize: 24, color: Colors.black)),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        backgroundColor: colorGold,
+      ));
+  static final ThemeData darkTheme = ThemeData();
+}
